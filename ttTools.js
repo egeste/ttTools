3eccc88f86e45c1880d6ff894ec647faa1af5906
Stevo Egesté <github@egeste.net>
2011-12-06 03:54:47 -0800

    Testing

--- ttTools.js
+++ ttTools.js
@@ -105,7 +105,7 @@ ttTools = {
   updateVotesOverride : function () {
     var room = this.getRoom();
     if (!room) { return false; }
-    if (!room.downvoters) { this.downvoters = []; }
+    if (!room.downvoters) { room.downvoters = []; }
     room.updateVotesFunc = room.updateVotes;
     room.updateVotes = function (votes, g) {
       this.updateVotesFunc(votes, g);
